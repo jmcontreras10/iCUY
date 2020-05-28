a59f1e2364e45a84a3a9d3853207061a724dc984
Matt <jm.contreras10@uniandes.edu.co>
2020-05-28 11:46:14 -0500

    Commit to proof

--- front/src/Components/Home.js
+++ front/src/Components/Home.js
@@ -6,6 +6,14 @@ import CuyGraph from './CuyGraph'
 function Home(props) {
 
     const [select, setSelect] = useState(0)
+    const [todayH, setTodayH] = useState(0);
+
+    useEffect(() => {
+        let d = new Date();
+        let actualDate = `${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}`;
+        let today = props.records.filter(ele => ele.date == actualDate);
+        setTodayH(today.lenght);
+    },[])
 
 
     return (
@@ -15,12 +23,12 @@ function Home(props) {
                     <h1 className="titulo-seccion">Registro tus logros diarios</h1>
                     <div className="accordion" id="DiaryAreas">
                         <DiaryCard area={0} user={props.user} habits={props.habits} records={props.records} />
-                        <DiaryCard area={1} user={props.user} habits={props.habits} records={props.records}/>
-                        <DiaryCard area={2} user={props.user} habits={props.habits} records={props.records}/>
+                        <DiaryCard area={1} user={props.user} habits={props.habits} records={props.records} />
+                        <DiaryCard area={2} user={props.user} habits={props.habits} records={props.records} />
                     </div>
                 </div>
                 <div className="col-md-6 col-12">
-                    <CuyGraph completed={10} total={30} />
+                    <CuyGraph completed={3} total={5} />
 
                 </div>
             </div>
@@ -30,7 +38,7 @@ function Home(props) {
 Home.propTypes = {
     user: PropTypes.object.isRequired,
     habits: PropTypes.array.isRequired,
-    records:PropTypes.array.isRequired
+    records: PropTypes.array.isRequired
 }
 
 export default Home;
